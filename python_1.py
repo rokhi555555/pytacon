e80119de5c5498a8043cf8e646badd90f8b16c44
Muawiya_sultan <ameermuawiya472@gmail.com>
2024-01-01 11:56:54 +0500

    python's 01 prog

---
+++ python_1.py
@@ -0,0 +1,7 @@
+maths = int(input("enter marks of a student in maths"))
+eng = int(input("enter marks of a student in eng"))
+phys = int(input("enter marks of a student in phys"))
+
+marks=[maths,eng,phys]
+print(marks)
+print("total marks obtained are :",marks[0]+marks[1]+marks[2])
